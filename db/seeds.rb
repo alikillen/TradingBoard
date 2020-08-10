1fa3a14da20dff72163ad7cb9caf3ee812baef9b
alikillen <alisoneme1994@gmail.com>
2020-08-10 20:59:25 +1000

    styled index show pages etc

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,10 @@
   name:Faker::Game.title,
   description:Faker::Movie.quote,
   #date_posted:Faker::Date.between(from: '2014-09-23', to: Date.today),
-  price_type: #random
+  price_type: #random of 3
+  price: #random, must be zero if price_type is free
   category: #random
-  price:
+  
   photo: #random from faker
   
 )
